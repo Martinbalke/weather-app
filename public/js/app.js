91ea6c6271a88ba44b2871f6575ee76aeda4961f
Martin Balke <Mrtnbalke@gmail.com>
2019-08-06 20:09:10 -0700

    heroku deployment

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ formEL.addEventListener('submit', (e) => {
 	e.preventDefault();
 	const location = searchEL.value;
 
-	fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+	fetch(`/weather?address=${location}`).then((response) => {
 		response.json().then((data) => {
 			if (data.error) {
 				render(data.error);
